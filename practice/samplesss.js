b654a45bcaa2ad9c615be33b76d47e77e0c9becf
Aneesh-aj <rajuaneesh.p2020@gmail.com>
2023-12-11 15:31:07 +0530

    removed commanded lines

--- practice/samplesss.js
+++ practice/samplesss.js
@@ -1,12 +1,10 @@
 const word ="word me";
 
-// let newword = word.split('')
 const anotherword = word.split(" ")[1]
 
-// console.log(newword)
 
 console.log(anotherword)
 
 const word1 = "my country";
 const dividedWord = word1.split(" ")[1];
-console.log(dividedWord); // Output: "country"
+console.log(dividedWord); 
